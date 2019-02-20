9de9b05d6a2b76c2392787bd7aabf6160450e4e5
tech-w93a <tmuparadzi@spartaglobal.com>
2019-02-20 02:07:30 +0000

    Xunit_Test done

---
+++ HOMEWORK/Tcalculator/ClassLibrary2/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorStuff
+{
+    public class Calculator
+    {
+        public double Add(double num1, double num2)
+        {
+            double result = num1 + num2;
+            return result;
+        }
+        public double Divide(double num1, double num2)
+        {
+            double result = num1 / num2;
+            return result;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            double result = num1 * num2;
+            return result;
+        }
+        public double Modula(double num1, double num2)
+        {
+            double result = num1 % num2;
+            return result;
+        }
+        public double Subtract(double num1, double num2)
+        {
+            double result = num1 - num2;
+            return result;
+        }
+        public double AreaOfCircle(double num1)
+        {
+            num1 = Math.PI;
+            double result = Math.PI;
+            return result;
+        }
+        public double AreaOfSquare(double num1)
+        {
+            double result = num1 * num1;
+            return result;
+        }
+        public double AreaOfTriangle(double BaseOfTriangle, double HeightOfTriangle)
+        {
+            double result = (BaseOfTriangle * HeightOfTriangle) / 2;
+            return result;
+        }
+
+    }
+}
---
+++ HOMEWORK/Tcalculator/Tcalculator/UnitTest1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using CalculatorStuff;
+
+namespace Tcalculator
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestAdd()
+        {   //Arrange
+            Calculator calc = new Calculator();
+            double expected = 6.0;
+            //Act
+            double actual = calc.Add(2.0, 4.0);
+            //Asserty
+            Assert.Equal(expected, actual);
+        }
+        [Fact]//Atributes
+        public void TestDivide()
+        {
+            Calculator calc = new Calculator();
+            double expected = 2.0;
+            //Act
+            double actual = calc.Divide(4.0, 2.0);
+            //Asserty
+            Assert.Equal(expected, actual);
+        }
+        [Fact]//Atributes
+        public void TestMultiply()
+        {
+            Calculator calc = new Calculator();
+            double expected = 6.0;
+            //Act
+            double actual = calc.Multiply(2.0, 3.0);
+            //Asserty
+            Assert.Equal(expected, actual);
+        }
+        [Fact]//Atributes
+        public void TestModula()
+        {
+            Calculator calc = new Calculator();
+            double expected = 2.0;
+            //Act
+            double actual = calc.Modula(2.0, 4.0);
+            //Asserty
+            Assert.Equal(expected, actual);
+        }
+        [Fact]//Atributes
+        public void TestSubtract()
+        {
+            Calculator calc = new Calculator();
+            double expected = 0.0;
+            //Act
+            double actual = calc.Subtract(4.0, 4.0);
+            //Asserty
+            Assert.Equal(expected, actual);
+        }
+        [Fact]//Atributes
+        public void TestAreaOfCircle()
+        {
+            Calculator calc = new Calculator();
+            double expected = Math.PI;
+            //Act
+            double actual = calc.AreaOfCircle(Math.PI);
+            //Asserty
+            Assert.Equal(expected, actual);
+        }
+        [Fact]//Atributes
+        public void TestAreaOfSquare()
+        {
+            Calculator calc = new Calculator();
+            double expected = 16.0;
+            //Act
+            double actual = calc.AreaOfSquare(4.0);
+            //Asserty
+            Assert.Equal(expected, actual);
+        }
+        [Fact]//Atributes
+        public void TestAreaOfTriangle()
+        {
+            Calculator calc = new Calculator();
+            double expected = 198.0;
+            //Act
+            double actual = calc.AreaOfTriangle(33.0, 12.0);
+            //Asserty
+            Assert.Equal(expected, actual);
+        }
+    }
+}
